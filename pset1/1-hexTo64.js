527656dfb9f706f8b0a0b8f7a8e779b849f9a755
unknown <jacob.e.england@gmail.com>
2020-11-08 18:25:18 -0500

    Added solution to pset1 problem 2

--- pset1/1-hexTo64.js
+++ pset1/1-hexTo64.js
@@ -1,5 +1,5 @@
-let input = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-let output = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+const input = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+const output = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
 // Function to convert a hex string to an array of bytes
 const hexToBase64 = (str) => {
---
+++ pset1/2-fixedXOR.js
@@ -0,0 +1,20 @@
+const input = '1c0111001f010100061a024b53535009181c'
+const key = '686974207468652062756c6c277320657965'
+const output = '746865206b696420646f6e277420706c6179'
+
+const fixedXOR = (input, key) => {
+    let inputBytes = Buffer.from(input, 'hex')
+    let keyBytes = Buffer.from(key, 'hex')
+    let results = []
+    for (let i = 0; i < inputBytes.length; i++) {
+        results.push(inputBytes[i] ^ keyBytes[i])
+    }
+    results = Buffer.from(results)
+    return results.toString('hex')
+}
+
+const test = (input, key, output) => {
+    return console.log(fixedXOR(input, key) == output)
+}
+
+test(input, key, output)
